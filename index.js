389466a7ffea00021da0c1179077924f8da53d7a
ThabangManaka <thabangdansteyn@gmail.com>
2020-08-10 04:07:22 -0700

    Adding media query

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import stylesheets
 import './myBooking.css';
+import './responsive.css';
 import 'bootstrap';
 import ('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
 
